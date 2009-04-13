2f920b2ce68e5b52ea24fd8a12a705e101465854
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-04-13 02:52:14 +0000

    Add gcc_support.c, which defines non-inlined memcpy/memset functions. Required for older versions of gcc that don't use intrinsics when copying or filling structs.

--- lib/types.h
+++ lib/types.h
@@ -69,37 +69,37 @@ typedef struct {
 static inline void
 memcpy(void *dest, const void *src, uint32 size)
 {
-   asm volatile ("rep movsb" : "+c" (size), "+S" (src), "+D" (dest) :: "memory");
+   asm volatile ("cld; rep movsb" : "+c" (size), "+S" (src), "+D" (dest) :: "memory");
 }
 
 static inline void
 memset(void *dest, uint8 value, uint32 size)
 {
-   asm volatile ("rep stosb" : "+c" (size), "+D" (dest) : "a" (value) : "memory");
+   asm volatile ("cld; rep stosb" : "+c" (size), "+D" (dest) : "a" (value) : "memory");
 }
 
 static inline void
 memcpy16(void *dest, const void *src, uint32 size)
 {
-   asm volatile ("rep movsw" : "+c" (size), "+S" (src), "+D" (dest) :: "memory");
+   asm volatile ("cld; rep movsw" : "+c" (size), "+S" (src), "+D" (dest) :: "memory");
 }
 
 static inline void
 memset16(void *dest, uint16 value, uint32 size)
 {
-   asm volatile ("rep stosw" : "+c" (size), "+D" (dest) : "a" (value) : "memory");
+   asm volatile ("cld; rep stosw" : "+c" (size), "+D" (dest) : "a" (value) : "memory");
 }
 
 static inline void
 memcpy32(void *dest, const void *src, uint32 size)
 {
-   asm volatile ("rep movsl" : "+c" (size), "+S" (src), "+D" (dest) :: "memory");
+   asm volatile ("cld; rep movsl" : "+c" (size), "+S" (src), "+D" (dest) :: "memory");
 }
 
 static inline void
 memset32(void *dest, uint32 value, uint32 size)
 {
-   asm volatile ("rep stosl" : "+c" (size), "+D" (dest) : "a" (value) : "memory");
+   asm volatile ("cld; rep stosl" : "+c" (size), "+D" (dest) : "a" (value) : "memory");
 }
 
 #define Atomic_Exchange(mem, reg) \
